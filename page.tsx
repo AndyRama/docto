c7c93e6bfa7fd9a50af195efdd43ed04f31d7e9e
andy ramaroson <andyramaroson@gmail.com>
2025-02-11 18:56:49 +0100

    fix  clean code

--- page.tsx
+++ page.tsx
@@ -506,7 +506,7 @@ export default function LandingPage() {
                       </Button>
                     </Link> */}
 
-                    <Link href="/appointments">
+                    <Link href="/booking">
                       <Button className="w-full mb-4" size="lg">
                       <span className='my-[-10px] mx-1'>
                       <Calendar/>
